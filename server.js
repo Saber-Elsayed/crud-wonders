7cb39c3aedfbb3f67d16dc78567e54613406b1a9
Saber-Elsayed <alsayyedsabe@gmail.com>
2023-12-14 15:56:47 +0200

    CRUD

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
+const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 const wonders = [
-    { name: "Mount Everest", location: "Nepal", visited: false },
-    { name: "Grand Canyon", location: "Arizona", visited: false },
-    { name: "Botanical Gardens", location: "Singapore", visited: true },
-    { name: "Pantheon", location: "Greece", visited: false },
-    { name: "Colosseum", location: "Italy", visited: true }
-]
+  { name: "Mount Everest", location: "Nepal", visited: false },
+  { name: "Grand Canyon", location: "Arizona", visited: false },
+  { name: "Botanical Gardens", location: "Singapore", visited: true },
+  { name: "Pantheon", location: "Greece", visited: false },
+  { name: "Colosseum", location: "Italy", visited: true },
+];
 
-app.get('/wonders', function (req, res) {
-    res.send(wonders)
-})
+app.get("/wonders", function (req, res) {
+  res.send(wonders);
+});
 
-const port = 1337 //because why not
+app.post("/wonder", function (req, res) {
+  console.log(req.body);
+  console.log("Someone's trying to make a post request");
+});
+
+const port = 1337; //because why not
 app.listen(port, function () {
-    console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
