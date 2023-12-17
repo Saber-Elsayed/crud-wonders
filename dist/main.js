d2076a78a24e5bc4b4aa16efd1753fd25ab3ad4e
Saber-Elsayed <alsayyedsabe@gmail.com>
2023-12-17 15:35:19 +0200

    fork add features

--- dist/main.js
+++ dist/main.js
@@ -18,13 +18,28 @@ const addWonder = function () {
   let newLocation = $("#new-location-input").val();
   //POST the newWonder to the server
   let data = { name: newWonder, location: newLocation };
+  console.log(data);
   $.post("/wonder", data, function (response) {
+    fetch();
     console.log("POST complete");
   });
 };
 
+const updateVisited = function (wonder) {
+  $.ajax({
+    url: `wonder/${wonder}`,
+    method: "PUT",
+    success: function (response) {
+      console.log("PUT complete");
+    },
+  });
+  fetch();
+};
+
 $("#wonders").on("click", ".visit", function () {
   let wonder = $(this).closest(".wonder").find(".name").text();
+  let wonderName = fullWonder.split("-")[0].trim();
+  updateVisited(wonderName);
   //PUT this to the server: update the wonder's `visited` status to `true`
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "node_modules")));
 
@@ -17,9 +20,27 @@ app.get("/wonders", function (req, res) {
   res.send(wonders);
 });
 
-app.post("/wonder", function (req, res) {
+app.put("/wonders/:name", function (req, res) {
+  const wonderName = req.params.name;
+  const foundWonder = wonders.find((wonder) => wonder.name === wonderName);
+
+  if (foundWonder) {
+    foundWonder.visited = true;
+
+    console.log(`Visited status updated for ${foundWonder.name}`);
+
+    res.send({ message: "Visited status updated successfully" });
+  }
+});
+
+app.post("/wonder/", function (req, res) {
+  const obj = req.body;
+  obj.visited = false;
+  wonders.push(obj);
   console.log(req.body);
+  console.log(wonders);
   console.log("Someone's trying to make a post request");
+  res.end;
 });
 
 const port = 1337; //because why not
